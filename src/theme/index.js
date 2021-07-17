5f931194c8b48586d65d857db9dda16b9acd2b30
Rabin Hansda <rabinhansda24@gmail.com>
2021-07-18 02:39:40 +0530

    fonts and color setup

---
+++ src/theme/index.js
@@ -0,0 +1,27 @@
+const theme = {
+    fonts: {
+        bold: "Poppins-Bold",
+        semiBold: "Poppins-SemiBold",
+        regular: "Poppins-Regular",
+        medium: "Poppins-Medium",
+        thin: "Poppins-Thin",
+        light: "Poppins-Light",
+        italic: "Poppins-Italic"
+    },
+    colors: {
+        primaryColor: "",
+        secondaryColor: "",
+        primaryText: "",
+        secondaryText: "",
+        labelColor: "",
+        placeholderColor: "",
+        inputBorder: "",
+        devider: "",
+        success: "",
+        error: "",
+        info: "",
+        warning: ""
+    }
+}
+
+export default theme
